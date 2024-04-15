d75d09bc5085394080e5092e121555096de717a6
Napasorn TEVARUT <napasorn.te@ku.th>
2024-04-15 23:25:44 +0700

    fix predict backend and html

--- ezdust/ezdust/urls.py
+++ ezdust/ezdust/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='dust:home',
+                                  permanent=False)),
     path('admin/', admin.site.urls),
     path('home/', include('dust.urls'))
 ]
